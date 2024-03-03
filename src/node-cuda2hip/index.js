b5b478792f774314feba3762a85f93b248f0e488
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-03-03 23:25:44 +0100

    Update index.js

--- src/node-cuda2hip/index.js
+++ src/node-cuda2hip/index.js
@@ -9,19 +9,48 @@ import fs from 'fs'
 import csv from 'csv-parser'
 
 function parseCSV(filePath) {
-    const results = [];
+    return new Promise((res) => {
+        const results = [];
 
-    fs.createReadStream(filePath)
-        .pipe(csv({ headers: ['cuda0', 'cuda1', 'cuda2', 'cuda3', 'cuda4', 'hip0', 'hip1', 'hip2', 'hip3', 'hip4'] }))
-        .on('data', (data) => results.push(data))
-        .on('end', () => {
-            // important cuda0 and hip0 (functions names)
-            console.log(results);
-            // Process results here
-        })
-        .on('error', (error) => {
-            console.error('Error reading the CSV file:', error);
-        });
+        fs.createReadStream(filePath)
+            .pipe(csv({ headers: ['cuda0', 'cuda1', 'cuda2', 'cuda3', 'cuda4', 'hip0', 'hip1', 'hip2', 'hip3', 'hip4'] }))
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                // important cuda0 and hip0 (functions names)
+                res(results)
+            })
+            .on('error', (error) => {
+                console.error('Error reading the CSV file:', error);
+            });
+    })
 }
 
-parseCSV('./input/cuda_runtime.csv');
+function readJson(path) {
+    let json = fs.readFileSync(path)
+    return JSON.parse(json)
+}
+
+function organizeCudaApi(api) {
+    let funs = {}
+    for (let m in api.modules) {
+        let mod = api.modules[m]
+        for (let fun of mod) {
+            funs[fun.name] = fun
+        }
+    }
+
+    return { functions: funs }
+}
+
+async function main() {
+    let hipify = await parseCSV('./input/cuda_runtime.csv');
+
+    let cudaApi = readJson('./input/cuda_api.json')
+    cudaApi = organizeCudaApi(cudaApi)
+
+    let hipApi = readJson('./input/hip_api.json')
+
+    console.log("check")
+}
+
+main()
