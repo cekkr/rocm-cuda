3deac123ff8b3737a06b3c7a255262ee6db87434
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-03-03 22:53:15 +0100

    beginning cuda2hip...

---
+++ src/node-cuda2hip/index.js
@@ -0,0 +1,8 @@
+/*
+This library uses a simple local http server that runs predictions using CodeLlama 13b model
+For confortability, this project could be found in a my personal git server:
+http://eswayer.com:3000/riccardo/codellama-center
+*/
+import { requestCodeLlama } from '../libs/node-codellama-request'
+
+
