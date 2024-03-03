ad4894c69ac8df5c41859651f3eaccf261975000
Riccardo Cecchini <16149805+cekkr@users.noreply.github.com>
2024-03-03 23:12:07 +0100

    csv

--- src/node-cuda2hip/index.js
+++ src/node-cuda2hip/index.js
@@ -3,6 +3,25 @@ This library uses a simple local http server that runs predictions using CodeLla
 For confortability, this project could be found in a my personal git server:
 http://eswayer.com:3000/riccardo/codellama-center
 */
-import { requestCodeLlama } from '../libs/node-codellama-request'
+import { requestCodeLlama } from '../libs/node-codellama-request/index.js'
+//import XLSX from 'xlsx'
+import fs from 'fs'
+import csv from 'csv-parser'
 
+function parseCSV(filePath) {
+    const results = [];
 
+    fs.createReadStream(filePath)
+        .pipe(csv({ headers: ['cuda0', 'cuda1', 'cuda2', 'cuda3', 'cuda4', 'hip0', 'hip1', 'hip2', 'hip3', 'hip4'] }))
+        .on('data', (data) => results.push(data))
+        .on('end', () => {
+            // important cuda0 and hip0 (functions names)
+            console.log(results);
+            // Process results here
+        })
+        .on('error', (error) => {
+            console.error('Error reading the CSV file:', error);
+        });
+}
+
+parseCSV('./input/cuda_runtime.csv');
